3ffd46c23c10642db25c6438415fa31b6619bad0
eric183 <kk297466058@gmail.com>
2022-09-08 01:01:26 +0800

    updated

--- nes-scaffold/src/tuple.rs
+++ nes-scaffold/src/tuple.rs
@@ -3,4 +3,8 @@ fn main() {
   let turple_a = ("e", 5i32, true);
 
   println!("console str {} with number {}, is {}", turple_a.0, turple_a.1, turple_a.2);
+
+
+  let a: [i32; 5] = [1, 2, 3, 4, 5];
+
 }
